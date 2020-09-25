814203c4f18b801affd4ae9f63a9ecb33af6e7c3
Symblox Dev <symblox@protonmail.com>
2020-09-25 17:40:36 +0800

    Add github icon and link

--- components/footer.js
+++ components/footer.js
@@ -1,28 +1,37 @@
 export default function Footer() {
-    return (
-        <footer>
-            <div className="container flex">
-                <div className="logo"></div>
-                <div className="r">
-                    <div className="menu">
-                        <a
-                            href="https://twitter.com/symbloxdefi"
-                            target="_blank"
-                        >
-                            <img src="/twitter.svg" alt="twitter" height="39" />
-                        </a>
-                        <a href="https://t.me/symblox" target="_blank">
-                            <img src="/terims.svg" alt="terims" height="39" />
-                        </a>
-                        <a
-                            href="https://medium.com/@symbloxsyx"
-                            target="_blank"
-                        >
-                            <img src="/media.svg" alt="media" height="39" />
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer>
+      <div className="container flex">
+        <div className="logo"></div>
+        <div className="r">
+          <div className="menu">
+            <a href="https://twitter.com/symbloxdefi" target="_blank">
+              <img src="/twitter.svg" alt="twitter" height="39" />
+            </a>
+            <a href="https://t.me/symblox" target="_blank">
+              <img src="/terims.svg" alt="terims" height="39" />
+            </a>
+            <a href="https://medium.com/@symbloxsyx" target="_blank">
+              <img src="/media.svg" alt="media" height="39" />
+            </a>
+            <a
+              href="https://github.com/symblox/symblox-yield-farming"
+              target="_blank"
+            >
+              <img
+                src={"/github.svg"}
+                alt="github"
+                style={{
+                  background: "#49475F",
+                  borderRadius: "50%",
+                  padding: "6px",
+                  height: "27px",
+                }}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
