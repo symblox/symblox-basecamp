d16ae5a008358af3b16c06322dd046c8ca721ac5
Symblox Dev <symblox@protonmail.com>
2020-10-19 23:46:30 +0800

    Added GA

--- components/meta.js
+++ components/meta.js
@@ -3,6 +3,15 @@ import Head from "next/head";
 export default function Meta() {
   return (
     <Head>
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-180843030-1"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-180843030-1');
+      </script>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
