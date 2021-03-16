bab2a0b27535e7b0ef0d4e145accb5126e4e6340
Symblox Dev <symblox@protonmail.com>
2021-03-16 21:29:16 +0800

    Updated android binary

--- pages/index.js
+++ pages/index.js
@@ -536,7 +536,7 @@ export default function Index() {
                     </a>
                   </div>
                   <div className="app-item">
-                    <a href="/defi-wallet-0210.apk" target="_blank">
+                    <a href="/defi-wallet-0314.apk" target="_blank">
                       <span className="app-2"></span>
                     </a>
                   </div>
